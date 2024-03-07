f98bb20ceef41464a74a29d6521e28c9b154fac5
Lucas <35976410+lucasnasc2@users.noreply.github.com>
2024-03-07 18:15:31 +0100

    Update createSymlink.sh

--- createSymlink.sh
+++ createSymlink.sh
@@ -5,6 +5,5 @@ firebaseAdminCLI_path="$PWD"
 
 # Create the symbolic link
 sudo ln -s "$firebaseAdminCLI_path/firebaseAdminCLI.sh" /usr/local/bin/firebaseAdmin
-sudo ln -s "$firebaseAdminCLI_path/index.js" /usr/local/bin/firebaseAdminIndex
 
 echo "Symbolic links created successfully."
