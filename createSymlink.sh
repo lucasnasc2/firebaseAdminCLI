c015423937aaaafd7431c2a4596d88700aa76a84
Lucas <35976410+lucasnasc2@users.noreply.github.com>
2024-03-08 11:00:50 +0100

    Update createSymlink.sh

--- createSymlink.sh
+++ createSymlink.sh
@@ -3,6 +3,13 @@
 # Get the current directory
 firebaseAdminCLI_path="$PWD"
 
+# Create the file and write the content
+cat <<EOT >> firebaseAdminCLI.sh
+#!/bin/bash
+
+node "$firebaseAdminCLI_path/index.js"
+EOT
+
 # Create the symbolic link
 sudo ln -s "$firebaseAdminCLI_path/firebaseAdminCLI.sh" /usr/local/bin/firebaseAdmin
 
